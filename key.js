81f5b051c5a586ebf48bf5c656828750ff285401
Gemdas <Brennan.artzt@gmail.com>
2017-10-08 10:18:17 -0500

    Coded

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'JViYBmBb8cB1Aon1oy1XOjyCv',
+  consumer_secret: 'BRr17Du0jdVnTWXE5Bx0krjrodtkmlMhrf3wzwymYRbgcww1mM',
+  access_token_key: '2687531078-zb6HvHBxobCmvb9vFYQ0MWiFj1WKwFSd1beA5Y8',
+  access_token_secret: 'gymzFQOcDyp1wPeCLo5VuTN89yPXu8tL6KXzeb0zR7HAL',
+}
+
+module.exports = twitterKeys;
